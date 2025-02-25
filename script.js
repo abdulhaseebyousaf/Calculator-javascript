c242c07a5859c0223af7c229a2885e059743594d
Sys-Engine <ahaseebyousaf@gmail.com>
2025-02-25 17:36:04 +0500

    check complete

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
+
 let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=> {
-button.addEventListener('click',(e)=> {
-    if(e.target.innerHTML === '='){
-        string = eval(string)
-        document.querySelector('input').value = string;
-    }
-    else if (e.target.innerHTML === 'C'){
-        string = ""
-        document.querySelector('input').value = string;
-    }
-    else {
-        string = string + e.target.innerHTML;
-        document.querySelector('input').value = string;
-    }
-    if (e.target.innerHTML === 'DEL'){
-        string = string.slice(0, -4);
-        document.querySelector('input').value = string;
-    }
-})
-})
+
+
+let buttons = document.querySelectorAll(".button");
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener("click", (e) => {
+        let value = e.target.innerHTML;
+
+        if (value === "=") {
+            try {
+                string = string.replace(/(^|[+\-*/])0+(\d+)/g, "$1$2");
+                string = eval(string).toString();
+                document.querySelector("input").value = string;
+            } catch {
+                string = "";
+                document.querySelector("input").value = "";
+            }
+        } else if (value === "C") {
+            string = "";
+            document.querySelector("input").value = string;
+        } else if (value === "DEL") {
+            string = string.slice(0, -1);
+            document.querySelector("input").value = string;
+        } else {
+            string += value;
+            document.querySelector("input").value = string;
+        }
+    });
+});
+
 function validateNumber(input) {
-    input.value = input.value.replace(/[^0-9,+,-,*,=,/]/g, ''); 
-  }
+    input.value = input.value.replace(/[^0-9+\-*/.]/g, "");
+}
